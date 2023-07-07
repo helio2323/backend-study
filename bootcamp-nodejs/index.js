a579d6d514980746a89436b4763068cba1583d6a
Helio Fernandes dos Reis Lima <helio_lima@outlook.com.br>
2023-07-06 22:30:34 -0300

    06.07

--- bootcamp-nodejs/index.js
+++ bootcamp-nodejs/index.js
@@ -1,5 +1,8 @@
 
-const fs = require('fs')
+const fs = require('fs');
+const http = require('http');
+
+
 
 
 //Blocking, synchronous way
@@ -16,8 +19,14 @@ const fs = require('fs')
 
 fs.readFile('./start.txt', 'utf-8', (err, data1) => {
     fs.readFile(`./${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data1)
         console.log(data2)
+        fs.readFile(`./${data2}.txt`, 'utf-8', (err, data3) => {
+            console.log(data3)
+
+            fs.writeFile('./final.txt', `${data2}\n${data3}`, 'utf-8', err =>{
+                console.log('Your file has been written :)')
+            })
+        })
     });
    
 });
